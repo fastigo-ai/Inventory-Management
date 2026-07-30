acecefb1de5dbaf488e97e0c54a4e4fdc2dfa626
sanjeet kumar <vermasanjeet240@gmail.com>
2026-07-30 18:49:49 +0530

    chore: remove temporary test scripts

--- backend/test-meta.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://fastigopvtltd_db_user:UpDQdSn25IPRy94R@cluster0.lgbl4nv.mongodb.net/?appName=Cluster0')
-  .then(async () => {
-    const db = mongoose.connection.db;
-    const meta = await db.collection('entitymetadatas').findOne({ name: 'Item' });
-    console.log(JSON.stringify(meta?.fields?.filter(f => f.label.includes('LOA')), null, 2));
-    process.exit(0);
-  });

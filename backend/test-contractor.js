d0bc54bebb54fa1c650da0a995e4d50433eb67e7
sanjeet kumar <vermasanjeet240@gmail.com>
2026-08-14 16:51:40 +0530

    chore: clean up debug and scratch scripts from repository

--- backend/test-contractor.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose.connect(process.env.MONGODB_URI).then(async () => {
-  const { Contractor } = require('./src/modules/contractors/contractor.schema');
-  const c = await Contractor.findOne();
-  console.log(JSON.stringify(c, null, 2));
-  process.exit(0);
-}).catch(console.error);
--- backend/test_csv.js
+++
@@ -1,4 +0,0 @@
-const { parseAndSanitizeCsv } = require('./dist/utils/csv.util.js');
-const buffer = Buffer.from('Name,TEMP CODE\nTest Name,12345\n"Test 2",67890');
-const parsed = parseAndSanitizeCsv(buffer);
-console.log(parsed);

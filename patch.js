ec76865605b9a4280736f8e43b8369cda2bb7dff
fastigo-ai <fastigo.in@gmail.com>
2026-08-21 14:08:10 +0530

    chore: cleanup test files

--- patch.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = './backend/src/modules/contractors/contractor.controller.ts';
-let code = fs.readFileSync(path, 'utf8');
-code = code.replace(
-  `export const bulkImportContractorReturns = asyncHandler(async (req: Request, res: Response) => {\n  if (!req.file) {`,
-  `export const bulkImportContractorReturns = asyncHandler(async (req: Request, res: Response) => {\n  console.log("Req headers:", req.headers["content-type"]);\n  console.log("Req file:", req.file);\n  console.log("Req body:", req.body);\n  if (!req.file) {`
-);
-fs.writeFileSync(path, code);

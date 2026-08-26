91da98261493dd23579fbd25c361366084c6fe03
fastigo-ai <fastigo.in@gmail.com>
2026-08-26 14:38:40 +0530

    chore: clean up temp test scripts

--- backend/src/scripts/test_build_query.ts
+++
@@ -1,9 +0,0 @@
-import { buildItemQueryAndSort } from './modules/items/item.controller';
-
-const queryCondition = buildItemQueryAndSort({
-  search: 'stay',
-  filter_circle: 'Solan',
-  filter_package: 'Package 1(S/N)'
-});
-
-console.log(JSON.stringify(queryCondition, null, 2));
